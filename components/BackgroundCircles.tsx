710621e4b7c75a619c170d058f2ef61fa9646619
Nick <nick.richarte@gmail.com>
2023-01-10 18:06:05 -0600

    background circles

--- components/BackgroundCircles.tsx
+++ components/BackgroundCircles.tsx
@@ -1,17 +1,17 @@
-import React from 'react'
+import React from "react";
 
-type Props = {}
+type Props = {};
 
 function BackgroundCircles({}: Props) {
-  return (
-    <div>
-        <div className="border border-red-500" />
-        <div />
-        <div />
-        <div />
-        <div />
-    </div>
-  )
+	return (
+		<div>
+			<div className="border [#333333] rounded-full h-[200px] w-[200px] mt-52 animate-ping " />
+			<div />
+			<div />
+			<div />
+			<div />
+		</div>
+	);
 }
 
-export default BackgroundCircles
+export default BackgroundCircles;
